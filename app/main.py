2cd5b2836191c58aa9405a1ba6eb43a574785924
uridonget <14010008@naver.com>
2025-08-13 11:14:48 +0900

    (ci/cd) ci/cd workflow added

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def health_check():
     return {"status": "ok"}
---
+++ tests/health_check.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+echo "--- Health Check Begin ---"
+
+echo "Waiting for server to start..."
+sleep 10
+
+echo "Send Request to http://localhost:8080/"
+RESPONSE=$(curl -s http://localhost:8080/)
+EXPECTED_RESPONSE='{"status":"ok"}'
+
+echo "Received response: $RESPONSE"
+
+if [ "$RESPONSE" == "$EXPECTED_RESPONSE" ]; then
+	echo "Health Check PASSED"
+	exit 0
+else
+	echo "Health Check FAILED"
+	exit 1
+fi
